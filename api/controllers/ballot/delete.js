744363cacd0b2df8585938e264e6978b91b24564
Outvoker <704457936@qq.com>
2018-06-27 22:48:00 +0800

    Delete casecade on ballot

--- api/controllers/ballot/delete.js
+++ api/controllers/ballot/delete.js
@@ -24,6 +24,32 @@ module.exports = {
 
   fn: async function (inputs, exits) {
 
+    let ballotOption = await Option.find({ ballot: inputs.id });
+
+    let i = 0;
+    for(i = 0; i < ballotOption.length; i++){
+
+      await Option.destroy(ballotOption[i].id)
+      .intercept(err => {
+        sails.log.error('Cannot destory option.');
+        sails.log.error(err);
+        return err;
+      });
+    }
+
+    let ballotrecord = await ballotrecord.find({ ballot: inputs.id });
+
+    let i = 0;
+    for(i = 0; i < ballotrecord.length; i++){
+
+      await Option.destroy(ballotrecord[i].id)
+      .intercept(err => {
+        sails.log.error('Cannot destory ballotrecord.');
+        sails.log.error(err);
+        return err;
+      });
+    }
+
     await Ballot.destroy(inputs)
     .intercept(err => {
       sails.log.error('Cannot destory ballot.');
--- api/controllers/option/delete.js
+++ api/controllers/option/delete.js
@@ -24,9 +24,22 @@ module.exports = {
 
   fn: async function (inputs, exits) {
 
-    await Option.destroy(inputs)
+    let ballotOption = await Option.find({ ballot: inputs.id });
+
+    let i = 0;
+    for(i = 0; i < ballotOption.length; i++){
+
+      await Option.destroy(ballotOption[i].id)
+      .intercept(err => {
+        sails.log.error('Cannot destory option.');
+        sails.log.error(err);
+        return err;
+      });
+    }
+
+    await Ballot.destroy(inputs)
     .intercept(err => {
-      sails.log.error('Cannot destory option.');
+      sails.log.error('Cannot destory ballot.');
       sails.log.error(err);
       return err;
     });
